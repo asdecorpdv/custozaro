d90b50a8c64e4ff240e595a9badb64b6e698576b
asdecorpdv <asdecorpdv@gmail.com>
2026-09-17 17:26:44 -0300

    Connect ForgeCost to Supabase

---
+++ src/supabase-config.js
@@ -0,0 +1,5 @@
+// Public client configuration; never put a secret/service_role key here.
+window.FORGE_SUPABASE = {
+  url: 'https://rgvgacnsvjakblvmpjgo.supabase.co',
+  publishableKey: 'sb_publishable_6QV0R79kEDBQhilLxT0mkw_QfILQiLB'
+};
